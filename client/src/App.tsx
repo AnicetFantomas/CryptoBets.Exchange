ef09f210e89d277f05f046d02ca4d76fe53f10eb
rolandm99 <manfulmweze99@gmail.com>
2022-12-21 15:24:50 +0200

    Install the dotenv dependency in the project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,40 @@
 import Layout from './layout/Layout';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, goerli, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
-function App() {
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, goerli, optimism, arbitrum],
+  [
+    alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_ID! }),
+    publicProvider()
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+const App = () => {
   return (
-    <Layout/>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Layout />
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
-}
+};
 
 export default App;
