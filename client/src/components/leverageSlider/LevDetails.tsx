5b784bc530b01a6298819f9bd41d7c8ba3c96629
Anicet Fantomas <anicetmurhula20@gmail.com>
2022-12-21 13:34:29 +0200

    Add laverage details

---
+++ client/src/components/leverageSlider/LevDetails.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const LevDetails = () => {
+  return (
+    <div className='w-full'>
+        <div className='flex w-full justify-between'>
+            <span>Available liquidity</span>
+            <span>3,873.56</span>
+        </div>
+        <div>
+            <span>Leverage</span>
+            <span>13.00x</span>
+        </div>
+        <div>
+            <span>Liq Price</span>
+            <span>$16,048.21</span>
+        </div>
+    </div>
+  )
+}
--- client/src/components/leverageSlider/levSlider.tsx
+++ client/src/components/leverageSlider/levSlider.tsx
@@ -1,28 +1,54 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
+import { LevDetails } from './LevDetails';
 
 function valuetext(value: number) {
-  return `${value}x`;
+    return `${value}x`;
 }
 
 const LevSlider = () => {
-  return (
-    <div className="mt-5 w-full justify-center">
-         <Box sx={{ width: 300 }}>
-      <Slider
-        aria-label="Temperature"
-        defaultValue={5}
-        getAriaValueText={valuetext}
-        valueLabelDisplay="auto"
-        step={5}
-        marks
-        min={5}
-        max={50}
-      />
-    </Box>
-    </div>
-  );
+    return (
+        <div className="m-5 w-60 bg-gray-200 justify-center">
+            <div className='w-full flex'>
+                <div className='mr-11 flex flex-col items-center'>
+                    <div>0</div>
+                </div>
+                <div className='mr-10 flex flex-col items-center'>
+                    <div>10</div>
+                </div>
+                <div className='mr-10 flex flex-col items-center'>
+                    <div>20</div>
+                </div>
+                <div className='mr-11 flex flex-col items-center'>
+                    <div>30</div>
+                </div>
+                <div className='mr-10 flex flex-col items-center'>
+                    <div>40</div>   
+                </div>
+                <div className='mr-11 flex flex-col items-center'>
+                    <div>50</div>
+                </div>
+            </div>
+            <div className='w-ful '>
+                <Box sx={{ width: 300 }}>
+                    <Slider
+                        aria-label="Temperature"
+                        defaultValue={0}
+                        getAriaValueText={valuetext}
+                        valueLabelDisplay="auto"
+                        step={10}
+                        marks
+                        min={0}
+                        max={50}
+                    />
+                </Box>
+            </div>
+            <div>
+                <LevDetails />
+            </div>
+        </div>
+    );
 }
 
 export default LevSlider;
