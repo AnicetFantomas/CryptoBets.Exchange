8e38b4a01ddd58f7504017ad71cd520e9dc0fa2b
rolandm99 <manfulmweze99@gmail.com>
2023-01-11 09:45:07 +0200

    Refactor code

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" 
                 fill="#FFFFFF" 
                 viewBox="0 0 30 30" 
-                stroke-width="1.5" 
+                strokeWidth="1.5" 
                 stroke="currentColor" 
-                class="menu mt-3 ml-6 w-14 h-15">
-                <path stroke-linecap="round" 
-                stroke-linejoin="round" 
+                className="menu mt-3 ml-6 w-14 h-15">
+                <path strokeLinecap="round" 
+                strokeLinejoin="round" 
                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
                 <p id="txt-title" className="text-white tracking-tighter mt-5 font-medium text-2xl">CryptoBets.Exchange</p>
