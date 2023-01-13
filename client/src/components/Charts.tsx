56a0007c109fddfc75e5a44c4d11c6f6b88246d4
rolandm99 <manfulmweze99@gmail.com>
2023-01-13 09:21:15 +0200

    Make the charts end-to-end

--- client/src/components/Charts.tsx
+++ client/src/components/Charts.tsx
@@ -3,7 +3,7 @@ import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
 
 const Charts = () => {
   return (
-    <div id="my-container" className="container rounded-3xl mt-8 p-4 pt-6 pb-6">
+    <div id="my-container" className="container rounded-3xl mt-8 pt-4 pb-6">
     <AdvancedRealTimeChart width={600} autosize symbol="ETHUSDT" theme="dark"/>
   </div>
   )
