8b337e13fe90e0a42d3cf07aecf5ae7379abaae1
rolandm99 <manfulmweze99@gmail.com>
2023-01-13 09:22:56 +0200

    Update the leverage slider

--- client/src/components/leverageSlider/levSlider.tsx
+++ client/src/components/leverageSlider/levSlider.tsx
@@ -1,52 +1,73 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import { LevDetails } from './LevDetails';
 
-function valuetext(value: number) {
+export function valuetext(value: number) {
     return `${value}x`;
 }
 
 const LevSlider = () => {
+
+    const [sliderValue, setSliderValue] =useState<any | null>(null);
+
     return (
         <div className="m-5 flex flex-col ">
             <h2 className='self-start text-white my-5'>Leverage</h2>
-            <div className=' flex self-center'>
-                <div className='mx-11 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+            <div className='flex self-center'>
+                <div className='mx-8 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
                     <span className='text-white'>0</span>
                 </div>
-                <div className='mr-10 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+                <div className='mr-6 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+                    <span className='text-white'>5</span>
+                </div>
+                <div className='mr-6 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
                     <span className='text-white'>10</span>
                 </div>
-                <div className='mr-10 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+                <div className='mr-6 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+                    <span className='text-white'>15</span>
+                </div>
+                <div className='mr-6 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
                     <span className='text-white'>20</span>
                 </div>
-                <div className='mr-11 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+                <div className='mr-6 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+                    <span className='text-white'>25</span>
+                </div>
+                <div className='mr-4 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
                     <span className='text-white'>30</span>
                 </div>
-                <div className='mr-10 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+                <div className='mr-6 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+                    <span className='text-white'>35</span>
+                </div>
+                <div className='mr-5 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
                     <span className='text-white'>40</span>   
                 </div>
-                <div className='mr-11 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+                <div className='mr-5 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
+                    <span className='text-white'>45</span>
+                </div>
+                <div className='mr-6 flex flex-col items-center hover:text-blue-200 cursor-pointer'>
                     <span className='text-white'>50</span>
                 </div>
             </div>
             <div className='flex self-center'>
-                <Box sx={{ width: 300 }}>
+                <Box sx={{ width: 400 }}>
                     <Slider
                         aria-label="Temperature"
                         defaultValue={0}
                         getAriaValueText={valuetext}
                         valueLabelDisplay="auto"
-                        step={10}
+                        step={5}
                         marks
                         min={0}
                         max={50}
+                        value={sliderValue}
+                        onChange={(event, newValue) => setSliderValue(newValue)}
+                        onChangeCommitted={(event, newValue) => setSliderValue(newValue)}
                     />
                 </Box>
             </div>
             <div>
-                <LevDetails />
+                <LevDetails sliderValue={sliderValue} setSliderValue={setSliderValue}  />
             </div>
                 
             
