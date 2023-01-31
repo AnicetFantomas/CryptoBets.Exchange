c145fdba52dc379c7f0497524768a0cd387c94a9
rolandm99 <manfulmweze99@gmail.com>
2023-01-31 13:47:52 +0200

    Retrieve data through the API and display

--- client/src/components/Tabs/Position.tsx
+++ client/src/components/Tabs/Position.tsx
@@ -4,16 +4,32 @@ import AppModal from '../GlobalModal/AppModal'
 import { ConfirmCancel } from './ConfirmCancel'
 
 const Orders = (props: any) => {
-  const myPositions = props.data
   const [show, setShow] = useState(false)
   const [id, setId] = useState(0)
+  const [allOrders, setAllOrders] = useState([]);
 
-  console.log('This is the data from the orders component: ', myPositions)
+  const getUrl = 'http://localhost:3002/api/orders'
 
   const handleCancel = (id: any) => {
     setId(id)
     setShow(true)
   }
+
+  const getOrders = async () => {
+    try {
+      const response = await fetch(getUrl)
+      const data = await (await response).json()
+      setAllOrders(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  console.log(allOrders);
+
+  useEffect(() => {
+    getOrders()
+  }, []);
 
   return (
     <>
@@ -27,7 +43,7 @@ const Orders = (props: any) => {
       <div className="flex justify-center w-full">
         <div className="flex flex-col w-full">
           <div className="flex flex-col w-full text-gray-200 ">
-            {myPositions.length === 0 ? (
+            {allOrders.length === 0 ? (
               <div className="text-xl font-bold text-red-600">
                 No open positions yet !
               </div>
@@ -40,23 +56,23 @@ const Orders = (props: any) => {
                     <td className="pl-8 whitespace-nowrap">Entry Price</td>
                   </tr>
 
-                  {myPositions.map((position: any) => (
+                  {allOrders.map((position: any) => (
                     <tr
                       key={position._id}
                       className="text-gray-500 my-0.5 text-center hover:text-gray-200"
                     >
                       <td className="whitespace-nowrap ">
-                        {position._amountIn}
+                        {position.amountIn}
                       </td>
                       <td className="pl-8 whitespace-nowrap">
-                        {position._sizeDelta}
+                        {position.sizeDelta}
                       </td>
                       <td className="pl-8 whitespace-nowrap">
-                        {position._acceptablePrice * Math.pow(10, -40)}
+                        {position.acceptablePrice * Math.pow(10, -40)}
                       </td>
                       <td className="pl-8 ">
                         <button
-                          onClick={() => handleCancel(position.id)}
+                          onClick={() => handleCancel(position._id)}
                           className="p-2 my-2 font-semibold text-gray-200 transition duration-500 bg-red-500 rounded-lg hover:bg-slate-700"
                         >
                           Close
